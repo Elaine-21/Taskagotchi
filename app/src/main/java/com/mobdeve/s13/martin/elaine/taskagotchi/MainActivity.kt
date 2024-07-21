ef68be42377d6c75f58ce434d9b5e245abda28b0
Elaine-21 <elainemartin022@gmail.com>
2024-07-21 14:26:17 +0800

    initial commit

--- app/src/main/java/com/mobdeve/s13/martin/elaine/taskagotchi/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s13/martin/elaine/taskagotchi/MainActivity.kt
@@ -1,25 +1,15 @@
 package com.mobdeve.s13.martin.elaine.taskagotchi
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.graphics.Typeface
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.mobdeve.s13.martin.elaine.taskagotchi.ui.theme.TaskagotchiTheme
-import java.util.logging.Handler
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //Creating splash screen or loading screen
@@ -28,18 +18,26 @@ class MainActivity : ComponentActivity() {
 
         setContentView(R.layout.activity_main)
 
+        // Getting all the preloaded font styles
+        val luckiest_guy: Typeface? = ResourcesCompat.getFont(this, R.font.luckiest_guy)
+        val lexend_medium: Typeface? = ResourcesCompat.getFont(this, R.font.lexend_medium)
+        val lexend_light: Typeface? = ResourcesCompat.getFont(this, R.font.lexend_light)
+        val lexend_extralight: Typeface? = ResourcesCompat.getFont(this, R.font.lexend_extralight)
+
         // Applying luckiest_guy font to text views
         val welcome_tv: TextView = findViewById(R.id.welcome_tv)
         val title_tv: TextView = findViewById(R.id.title_tv)
-        val luckiest_guy: Typeface? = ResourcesCompat.getFont(this, R.font.luckiest_guy)
         welcome_tv.typeface = luckiest_guy
         title_tv.typeface = luckiest_guy
 
         // Applying lexend_medium font to text views
         val username_tv: TextView = findViewById(R.id.username_tv)
-        val lexend_medium: Typeface? = ResourcesCompat.getFont(this, R.font.lexend_medium)
+        val password_tv: TextView = findViewById(R.id.pass_tv)
         username_tv.typeface = lexend_medium
+        password_tv.typeface = lexend_medium
 
 
     }
+
+
 }
