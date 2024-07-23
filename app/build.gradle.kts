c8fdb455f8464adcfe033fe63957a7078519a249
Elaine-21 <elainemartin022@gmail.com>
2024-07-23 23:20:07 +0800

    Firebase set up done Added Login and Register layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -38,6 +39,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -64,6 +66,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/mobdeve/s13/martin/elaine/taskagotchi/LoginActivity.kt
+++ app/src/main/java/com/mobdeve/s13/martin/elaine/taskagotchi/LoginActivity.kt
@@ -1,25 +1,28 @@
 package com.mobdeve.s13.martin.elaine.taskagotchi
 
-import android.annotation.SuppressLint
 import android.graphics.Typeface
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kotlin.math.sign
 
-class MainActivity : ComponentActivity() {
-    @SuppressLint("MissingInflatedId")
+class LoginActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //Creating splash screen or loading screen
         Thread.sleep(3000)
         installSplashScreen()
 
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         // Getting all the preloaded font styles
         val luckiest_guy: Typeface? = ResourcesCompat.getFont(this, R.font.luckiest_guy)
+        val lexend: Typeface? = ResourcesCompat.getFont(this, R.font.lexend)
         val lexend_medium: Typeface? = ResourcesCompat.getFont(this, R.font.lexend_medium)
         val lexend_light: Typeface? = ResourcesCompat.getFont(this, R.font.lexend_light)
         val lexend_extralight: Typeface? = ResourcesCompat.getFont(this, R.font.lexend_extralight)
@@ -31,12 +34,26 @@ class MainActivity : ComponentActivity() {
         title_tv.typeface = luckiest_guy
 
         // Applying lexend_medium font to text views
-        val username_tv: TextView = findViewById(R.id.username_tv)
-        val password_tv: TextView = findViewById(R.id.pass_tv)
+        val username_tv: TextView = findViewById(R.id.login_username_tv)
+        val password_tv: TextView = findViewById(R.id.login_pass_tv)
         username_tv.typeface = lexend_medium
         password_tv.typeface = lexend_medium
 
+        // Applying lexend_light font to text views
+        val login_username_input: EditText = findViewById(R.id.login_username_input)
+        val login_pass_input: EditText = findViewById(R.id.login_pass_input)
+        val sign_up_tv: TextView = findViewById(R.id.sign_up_now)
+        login_username_input.typeface = lexend_light
+        login_pass_input.typeface = lexend_light
+        sign_up_tv.typeface = lexend_light
 
+        // Applying lexend_extralight font to text views
+        val textView5: TextView = findViewById(R.id.textView5)
+        textView5.typeface = lexend_extralight
+
+        // Applying lexend font to button
+        val signIn_btn: Button = findViewById(R.id.signIn_btn)
+        signIn_btn.typeface = lexend
     }
 
 
