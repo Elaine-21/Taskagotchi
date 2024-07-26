55e0256eeb2abfe4693b54d11486bbc92ea493c5
dylanUSLD <160611650+dylanUSLD@users.noreply.github.com>
2024-07-27 03:36:34 +0800

    misc.xml JDK Version error

---
+++ app/src/main/java/com/mobdeve/s13/martin/elaine/taskagotchi/model/characterData.kt
@@ -0,0 +1,10 @@
+package com.mobdeve.s13.martin.elaine.taskagotchi.model
+
+data class characterData {
+
+    val name: String,
+    val status: String? = null,
+    val streak: Int? = null,
+    val energy: Int? = null
+
+}
