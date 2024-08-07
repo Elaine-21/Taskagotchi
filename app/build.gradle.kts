96e3ede95f39859f411d3be51f67bdb97ee0d8fb
dylanUSLD <160611650+dylanUSLD@users.noreply.github.com>
2024-08-07 20:38:32 +0800

    added incomplete activity_c

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.firebase.database)
+    implementation(libs.androidx.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
